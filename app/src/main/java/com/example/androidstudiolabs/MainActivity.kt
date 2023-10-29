a1069a52cec88be3e1b388a01e0a04a7eefe4976
OxxxyGeN <valeratoltek@gmail.com>
2023-10-29 19:25:34 +0300

    Metanit

--- app/src/main/java/com/example/androidstudiolabs/MainActivity.kt
+++ app/src/main/java/com/example/androidstudiolabs/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.example.androidstudiolabs
 
-import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -8,19 +7,19 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.unit.sp
 import androidx.compose.foundation.clickable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 
 class MainActivity : ComponentActivity() {
-    @SuppressLint("UnrememberedMutableState")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val message = mutableStateOf("Hello Jetpack")
+            val (value, setValue) = remember{mutableStateOf("Hello Jetpack")}
 
             Text(
-                text = message.value,
+                text = value,
                 fontSize = 28.sp,
-                modifier = Modifier.clickable( onClick = { message.value = "Hello Work!" })
+                modifier = Modifier.clickable( onClick = { setValue("Hello Metanit.com")})
             )
         }
     }
