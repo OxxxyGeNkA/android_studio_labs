b0e58c488136065b57c994421b10e8761e6f543d
OxxxyGeN <valeratoltek@gmail.com>
2023-11-02 16:06:48 +0300

    Metanit

--- app/src/main/java/com/example/androidstudiolabs/MainActivity.kt
+++ app/src/main/java/com/example/androidstudiolabs/MainActivity.kt
@@ -3,41 +3,44 @@ package com.example.androidstudiolabs
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.androidstudiolabs.ui.theme.AndroidStudioLabsTheme
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            AndroidStudioLabsTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+            val isAdded = remember{ mutableStateOf(false) }
+            Scaffold(
+                topBar = { TopAppBar({Text("METANIT.COM", fontSize = 22.sp)})},
+                bottomBar = {
+                    BottomAppBar{
+                        IconButton(onClick = {  }) { Icon(Icons.Filled.Menu, contentDescription = "Меню")}
+                        Spacer(Modifier.weight(1f, true))
+                        IconButton(onClick = {  }) { Icon(Icons.Filled.Search, contentDescription = "Поиск")}
+                    }
+                },
+                floatingActionButtonPosition = FabPosition.Center,
+                floatingActionButton = {
+                    FloatingActionButton(
+                        content = {
+                            if(isAdded.value) Icon(Icons.Filled.Clear, contentDescription = "Удалить")
+                            else Icon(Icons.Filled.Add, contentDescription = "Добавить") },
+                        onClick = { isAdded.value = !isAdded.value}
+                    )
                 }
+            ){
+                    innerPadding -> Text(text = if(isAdded.value) "Товар добавлен" else "Корзина пуста", fontSize = 28.sp, modifier = Modifier.padding(innerPadding))
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-            text = "Hello $name!",
-            modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AndroidStudioLabsTheme {
-        Greeting("Android")
     }
 }
