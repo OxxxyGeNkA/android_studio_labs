3d9c809963f974ec320de4557196ea01e25032de
OxxxyGeN <valeratoltek@gmail.com>
2023-11-02 17:06:35 +0300

    Metanit

--- app/src/main/java/com/example/androidstudiolabs/MainActivity.kt
+++ app/src/main/java/com/example/androidstudiolabs/MainActivity.kt
@@ -3,41 +3,52 @@ package com.example.androidstudiolabs
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.androidstudiolabs.ui.theme.AndroidStudioLabsTheme
+import androidx.compose.ui.unit.sp
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            AndroidStudioLabsTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+            val drawerState = rememberDrawerState(DrawerValue.Closed)
+            val scope = rememberCoroutineScope()
+            val selectedItem = remember{ mutableStateOf("")}
+            val items = listOf("Главная", "Контакты", "О приложении")
+            ModalNavigationDrawer(
+                drawerState = drawerState,
+                drawerContent={
+                    ModalDrawerSheet {
+                        for (item in items) {
+                            Text(
+                                item,
+                                fontSize = 28.sp,
+                                modifier = Modifier.clickable {
+                                    selectedItem.value = item
+                                    scope.launch { drawerState.close() }
+                                }
+                            )
+                        }
+                    }
+                }
+            ){
+                Column{
+                    Button(onClick = {
+                        scope.launch{ drawerState.open() }
+                    }) {
+                        Text("Меню", fontSize = 28.sp)
+                    }
+                    Text("Выбран пункт: ${selectedItem.value}", fontSize = 28.sp)
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-            text = "Hello $name!",
-            modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AndroidStudioLabsTheme {
-        Greeting("Android")
     }
 }
