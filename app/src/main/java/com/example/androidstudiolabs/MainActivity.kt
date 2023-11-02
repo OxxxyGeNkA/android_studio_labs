5944c81627adc6a1ee51dbd548050815b229b2a9
OxxxyGeN <valeratoltek@gmail.com>
2023-11-02 15:07:00 +0300

    Metanit

--- app/src/main/java/com/example/androidstudiolabs/MainActivity.kt
+++ app/src/main/java/com/example/androidstudiolabs/MainActivity.kt
@@ -3,28 +3,44 @@ package com.example.androidstudiolabs
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.selection.selectable
 import androidx.compose.foundation.selection.selectableGroup
 import androidx.compose.material3.RadioButton
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val state = remember { mutableStateOf(true) }
-            Column(Modifier.selectableGroup())
-            {
-                RadioButton(
-                    selected = state.value,
-                    onClick = { state.value = true }
-                )
-                RadioButton(
-                    selected = !state.value,
-                    onClick = { state.value = false }
-                )
+            val languages = listOf("Kotlin", "Java", "Javascript", "Rust")
+            val (selectedOption, onOptionSelected) = remember { mutableStateOf(languages[0]) }
+
+            Column(Modifier.selectableGroup()) {
+                languages.forEach { text ->
+                    Row(
+                        Modifier
+                            .fillMaxWidth()
+                            .height(56.dp)
+                            .selectable(
+                                selected = (text == selectedOption),
+                                onClick = { onOptionSelected(text) }
+                            ),
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        RadioButton(
+                            selected = (text == selectedOption),
+                            onClick = null
+                        )
+                        Text( text = text, fontSize = 22.sp )
+                    }
+                }
             }
         }
     }
