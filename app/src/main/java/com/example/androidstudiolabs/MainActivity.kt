7d9fc999cf07c2afc66fa73014ba5abece744992
OxxxyGeN <valeratoltek@gmail.com>
2023-11-02 15:10:01 +0300

    Metanit

--- app/src/main/java/com/example/androidstudiolabs/MainActivity.kt
+++ app/src/main/java/com/example/androidstudiolabs/MainActivity.kt
@@ -3,41 +3,51 @@ package com.example.androidstudiolabs
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material3.IconToggleButton
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.androidstudiolabs.ui.theme.AndroidStudioLabsTheme
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            AndroidStudioLabsTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+            Column{
+                IconButton(onClick = {  }) {
+                    Icon(
+                        Icons.Filled.Info,
+                        contentDescription = "Информация о приложении", modifier = Modifier.size(80.dp),
+                        tint = Color.Red
+                    )
+                }
+
+                val checked = remember { mutableStateOf(false) }
+                Row(verticalAlignment = Alignment.CenterVertically){
+                    IconToggleButton(checked = checked.value, onCheckedChange = { checked.value = it }) {
+                        Icon(
+                            Icons.Filled.Info,
+                            contentDescription = "Информация о приложении",
+                            tint = if (checked.value) Color(0xFFEC407A) else Color(0xFFB0BEC5)
+                        )
+                    }
+                    Text(
+                        text = if(checked.value) "Выбрано" else "Не выбрано",
+                        fontSize = 28.sp
+                    )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-            text = "Hello $name!",
-            modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AndroidStudioLabsTheme {
-        Greeting("Android")
     }
 }
