d7f4fe0c548c6bbf49c57ebee45347d0ab3e93aa
OxxxyGeN <valeratoltek@gmail.com>
2023-11-02 18:08:30 +0300

    Metanit

--- app/src/main/java/com/example/androidstudiolabs/MainActivity.kt
+++ app/src/main/java/com/example/androidstudiolabs/MainActivity.kt
@@ -3,41 +3,52 @@ package com.example.androidstudiolabs
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.androidstudiolabs.ui.theme.AndroidStudioLabsTheme
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            AndroidStudioLabsTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+            Column{
+                Text(
+                    text = stringResource(R.string.app_name),
+                    fontSize = 28.sp
+                )
+                Text(
+                    text = stringResource(R.string.message),
+                    fontSize = 28.sp
+                )
+                Text(
+                    text = stringResource(R.string.welcome),
+                    fontSize = 28.sp
+                )
+                Button(onClick={}){
+                    Text(
+                        text = stringResource(R.string.click_button),
+                        fontSize = 22.sp
+                    )
                 }
+                Text(
+                    text = stringResource(R.string.user_data, "Tom", 37, "JetBrains"),
+                    fontSize = 28.sp
+                )
+                Text(
+                    text = stringResource(R.string.user_data, "Bob", 41, "Google"),
+                    fontSize = 28.sp
+                )
+                /*Text(
+                    text = resources.getQuantityString(R.plurals.flowers, 21, 21),
+                    fontSize = 22.sp
+                )
+                Text(
+                    text = resources.getQuantityString(R.plurals.flowers, 7, 7),
+                    fontSize = 22.sp
+                )*/
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-            text = "Hello $name!",
-            modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AndroidStudioLabsTheme {
-        Greeting("Android")
     }
 }
