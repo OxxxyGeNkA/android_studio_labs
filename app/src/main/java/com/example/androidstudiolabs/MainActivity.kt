337488e88672504ed9828b4057fed0c03cd68aaa
OxxxyGeN <valeratoltek@gmail.com>
2023-11-02 15:00:29 +0300

    Metanit

--- app/src/main/java/com/example/androidstudiolabs/MainActivity.kt
+++ app/src/main/java/com/example/androidstudiolabs/MainActivity.kt
@@ -3,41 +3,29 @@ package com.example.androidstudiolabs
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.selection.selectableGroup
+import androidx.compose.material3.RadioButton
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.androidstudiolabs.ui.theme.AndroidStudioLabsTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            AndroidStudioLabsTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
-                }
+            val state = remember { mutableStateOf(true) }
+            Column(Modifier.selectableGroup())
+            {
+                RadioButton(
+                    selected = state.value,
+                    onClick = { state.value = true }
+                )
+                RadioButton(
+                    selected = !state.value,
+                    onClick = { state.value = false }
+                )
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-            text = "Hello $name!",
-            modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AndroidStudioLabsTheme {
-        Greeting("Android")
     }
 }
