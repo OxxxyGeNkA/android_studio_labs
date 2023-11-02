4147d99197b77e0c869073121a16037f3e887ba3
OxxxyGeN <valeratoltek@gmail.com>
2023-11-02 17:38:52 +0300

    Metanit

--- app/src/main/java/com/example/androidstudiolabs/MainActivity.kt
+++ app/src/main/java/com/example/androidstudiolabs/MainActivity.kt
@@ -3,41 +3,41 @@ package com.example.androidstudiolabs
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.androidstudiolabs.ui.theme.AndroidStudioLabsTheme
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            AndroidStudioLabsTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+            val checkedState = remember { mutableStateOf(false) }
+            val textColor = remember { mutableStateOf(Color.Unspecified) }
+            Column(modifier = Modifier.fillMaxSize().padding(10.dp)){
+                Row (verticalAlignment = Alignment.CenterVertically ){
+                    Text("Красный цвет", fontSize = 22.sp, color = textColor.value)
+                    Switch(
+                        checked = checkedState.value,
+                        onCheckedChange = {
+                            checkedState.value = it
+                            if(checkedState.value) textColor.value = Color(0xFFC62828)
+                            else textColor.value = Color.Unspecified
+                        },
+                        colors = SwitchDefaults.colors(
+                            checkedThumbColor = Color(0xFFC62828),
+                            checkedTrackColor = Color(0xFFE57373),
+                            uncheckedThumbColor = Color(0xFFEF9A9A),
+                            uncheckedTrackColor = Color(0xFFFFEBEE)
+                        )
+                    )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-            text = "Hello $name!",
-            modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AndroidStudioLabsTheme {
-        Greeting("Android")
     }
 }
