b544a20d8c6901458271635832d4cf121e673d2e
OxxxyGeN <valeratoltek@gmail.com>
2023-11-02 14:27:07 +0300

    Metanit

--- app/src/main/java/com/example/androidstudiolabs/MainActivity.kt
+++ app/src/main/java/com/example/androidstudiolabs/MainActivity.kt
@@ -5,8 +5,10 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.sp
 
@@ -16,10 +18,15 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             val message = remember{mutableStateOf("")}
+
             OutlinedTextField(
                 message.value,
                 {message.value = it},
                 textStyle = TextStyle(fontSize =  30.sp),
+                colors = TextFieldDefaults.outlinedTextFieldColors(
+                    focusedBorderColor= Color.Green, // цвет при получении фокуса
+                    unfocusedBorderColor = Color.LightGray  // цвет при отсутствии фокуса
+                )
             )
         }
     }
