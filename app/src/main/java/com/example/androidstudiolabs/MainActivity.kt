ed0e76e0ade21f667af6ca5de7afe7ebd78f654c
OxxxyGeN <valeratoltek@gmail.com>
2023-11-02 16:26:50 +0300

    Metanit

--- app/src/main/java/com/example/androidstudiolabs/MainActivity.kt
+++ app/src/main/java/com/example/androidstudiolabs/MainActivity.kt
@@ -3,41 +3,54 @@ package com.example.androidstudiolabs
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.androidstudiolabs.ui.theme.AndroidStudioLabsTheme
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            AndroidStudioLabsTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+            val scope = rememberCoroutineScope()
+            val snackbarHostState = remember { SnackbarHostState() }
+            val count = remember{ mutableStateOf(0) }
+            Scaffold(
+                snackbarHost = {
+                    SnackbarHost(snackbarHostState){ data ->
+                        Snackbar(
+                            modifier = Modifier.padding(10.dp),
+                            containerColor = Color(0xFF004D40),
+                            contentColor = Color(0xFFB2DFDB),
+                            action = {
+                                TextButton(onClick={ data.performAction() }){
+                                    Text("Add click", fontSize=22.sp, color=Color(0xFFB2DFDB))
+                                }
+                            }
+                        ){
+                            Text("Clicks count: ${count.value}", fontSize=26.sp)
+                        }
+                    }
+                }
+            ){
+                innerPadding ->
+                Button(
+                    onClick = {
+                        scope.launch {
+                            val result = snackbarHostState.showSnackbar("")
+                            if(result==SnackbarResult.ActionPerformed) count.value++
+                        }
+                    }
+                ){
+                    Text("Click", fontSize = 28.sp, modifier = Modifier.padding(innerPadding))
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-            text = "Hello $name!",
-            modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AndroidStudioLabsTheme {
-        Greeting("Android")
     }
 }
