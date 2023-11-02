e13fa4828581e8d092a01e5ac52bc7ab89510f06
OxxxyGeN <valeratoltek@gmail.com>
2023-11-02 18:02:49 +0300

    Metanit

--- app/src/main/java/com/example/androidstudiolabs/MainActivity.kt
+++ app/src/main/java/com/example/androidstudiolabs/MainActivity.kt
@@ -3,41 +3,66 @@ package com.example.androidstudiolabs
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.androidstudiolabs.ui.theme.AndroidStudioLabsTheme
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            AndroidStudioLabsTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+            var progress by remember { mutableStateOf(0.0f) }
+            val scope = rememberCoroutineScope()
+
+            var progress1 by remember { mutableStateOf(0.0f) }
+            val scope1 = rememberCoroutineScope()
+
+            Column{
+                Text("Статус: $progress", fontSize = 22.sp)
+                OutlinedButton(
+                    onClick = {
+                        scope.launch {
+                            while (progress < 1f) {
+                                progress += 0.0004f
+                                delay(1L)
+                            }
+                        }
+                    }
+                ) {
+                    Text("Запустить", fontSize = 22.sp)
                 }
+                CircularProgressIndicator(progress = progress)
+                Box{ CircularProgressIndicator() }
+
+                Text("Статус: $progress1", fontSize = 22.sp)
+                OutlinedButton(
+                    modifier = Modifier.padding(20.dp),
+                    onClick = {
+                        scope1.launch {
+                            while (progress1 < 1f) {
+                                progress1 += 0.0004f
+                                delay(1L)
+                            }
+                        }
+                    }
+                ) {
+                    Text("Запустить", fontSize = 22.sp)
+                }
+
+                LinearProgressIndicator(
+                    progress = progress1,
+                    color = Color(0xFFD32F2F),
+                    trackColor = Color(0xFFEF9A9A)
+                )
+
+                LinearProgressIndicator(modifier = Modifier.padding(20.dp))
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-            text = "Hello $name!",
-            modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AndroidStudioLabsTheme {
-        Greeting("Android")
     }
 }
