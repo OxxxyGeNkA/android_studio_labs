a90dc62ac74d50dcb1f20c9029695ae66cd30836
OxxxyGeN <valeratoltek@gmail.com>
2023-11-03 10:02:28 +0300

    Metanit

--- app/src/main/java/com/example/androidstudiolabs/MainActivity.kt
+++ app/src/main/java/com/example/androidstudiolabs/MainActivity.kt
@@ -19,7 +19,7 @@ class MainActivity : ComponentActivity() {
             AndroidStudioLabsTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                    Greeting("Ghoul")
                 }
             }
         }
