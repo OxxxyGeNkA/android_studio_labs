380e5f0301f4c48f99f642c3aa13c32580c4cbbb
OxxxyGeN <valeratoltek@gmail.com>
2023-11-03 11:11:17 +0300

    Metanit

--- app/src/main/java/com/example/androidstudiolabs/MainActivity.kt
+++ app/src/main/java/com/example/androidstudiolabs/MainActivity.kt
@@ -4,21 +4,24 @@ import android.media.MediaPlayer
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.runtime.remember
 import com.example.androidstudiolabs.ui.theme.DeadInside
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        val ghoulMusics = listOf(R.raw.imghoulfull, R.raw.imghoul)
         var ghoulPlayer = MediaPlayer.create(baseContext, R.raw.imghoulfull)
         ghoulPlayer.isLooping = true
         super.onCreate(savedInstanceState)
         setContent {
+            val curTrack = remember { mutableListOf(0) }
             var hz = DeadInside()
             if (hz && !ghoulPlayer.isPlaying) {
                 ghoulPlayer.start()
             }
             if (!hz && ghoulPlayer.isPlaying) {
                 ghoulPlayer.stop()
-                ghoulPlayer = MediaPlayer.create(baseContext, R.raw.imghoulfull)
+                ghoulPlayer = MediaPlayer.create(baseContext, ghoulMusics[0])
                 ghoulPlayer.isLooping = true
             }
         }
