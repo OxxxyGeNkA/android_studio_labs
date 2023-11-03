25324f46780278e735ecedde795870050f0a79cd
OxxxyGeN <valeratoltek@gmail.com>
2023-11-03 11:32:42 +0300

    Metanit

--- app/src/main/java/com/example/androidstudiolabs/MainActivity.kt
+++ app/src/main/java/com/example/androidstudiolabs/MainActivity.kt
@@ -4,25 +4,30 @@ import android.media.MediaPlayer
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import com.example.androidstudiolabs.ui.theme.DeadInside
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        val ghoulMusics = listOf(R.raw.imghoulfull, R.raw.imghoul)
+        val ghoulMusics = listOf(R.raw.imghoulfull, R.raw.femlove_zakat, R.raw.femlove_993, R.raw.femlove_pain, R.raw.kute_avoid_me)
         var ghoulPlayer = MediaPlayer.create(baseContext, R.raw.imghoulfull)
         ghoulPlayer.isLooping = true
         super.onCreate(savedInstanceState)
         setContent {
-            val curTrack = remember { mutableListOf(0) }
+            val curTrack = remember { mutableStateOf(0) }
             var hz = DeadInside()
             if (hz && !ghoulPlayer.isPlaying) {
+                ghoulPlayer = MediaPlayer.create(baseContext, ghoulMusics[curTrack.value])
                 ghoulPlayer.start()
+                curTrack.value += 1
+                if (curTrack.value == 5) {
+                    curTrack.value = 0
+                }
             }
             if (!hz && ghoulPlayer.isPlaying) {
                 ghoulPlayer.stop()
-                ghoulPlayer = MediaPlayer.create(baseContext, ghoulMusics[0])
-                ghoulPlayer.isLooping = true
+                //ghoulPlayer = MediaPlayer.create(baseContext, ghoulMusics[0])
             }
         }
     }
