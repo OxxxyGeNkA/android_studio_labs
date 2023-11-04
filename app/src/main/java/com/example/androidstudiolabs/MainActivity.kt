8523194a4202607e661eb063536d3b654582c267
OxxxyGeN <valeratoltek@gmail.com>
2023-11-04 18:18:32 +0300

    Metanit

--- app/src/main/java/com/example/androidstudiolabs/MainActivity.kt
+++ app/src/main/java/com/example/androidstudiolabs/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.androidstudiolabs
 
 import android.media.MediaPlayer
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.runtime.mutableStateOf
@@ -11,25 +12,34 @@ import com.example.androidstudiolabs.ui.theme.DeadInside
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         val ghoulMusics = listOf(R.raw.imghoulfull, R.raw.femlove_zakat, R.raw.femlove_993, R.raw.femlove_pain, R.raw.kute_avoid_me)
-        var ghoulPlayer = MediaPlayer.create(baseContext, R.raw.imghoulfull)
-        ghoulPlayer.isLooping = true
+        val fastMusic = listOf(R.raw.blyad_da_vi_ahueli, R.raw.zatknis_shluha, R.raw.smert, R.raw.pepsikolniy, R.raw.ya_siel_deda)
+        var ghoulPlayer = MediaPlayer.create(baseContext, ghoulMusics[0])
         super.onCreate(savedInstanceState)
         setContent {
             val curTrack = remember { mutableStateOf(0) }
+            val onPause = remember { mutableStateOf(false) }
             var hz = DeadInside()
-            ghoulPlayer.setOnCompletionListener {
-                ghoulPlayer.stop()
-            }
             if (hz && !ghoulPlayer.isPlaying) {
-                ghoulPlayer = MediaPlayer.create(baseContext, ghoulMusics[curTrack.value])
-                ghoulPlayer.start()
-                curTrack.value += 1
-                if (curTrack.value == 5) {
-                    curTrack.value = 0
+                if (!onPause.value) {
+                    ghoulPlayer = MediaPlayer.create(baseContext, ghoulMusics[curTrack.value])
                 }
+                //ghoulPlayer = MediaPlayer.create(baseContext, fastMusic[curTrack.value])
+                ghoulPlayer.start()
+                ghoulPlayer.setOnCompletionListener {
+                    curTrack.value += 1
+                    if (curTrack.value == 5) {
+                        curTrack.value = 0
+                    }
+                    onPause.value = false
+                }
+                //Toast.makeText(this, "Play", Toast.LENGTH_SHORT).show()
             }
+
+
             if (!hz && ghoulPlayer.isPlaying) {
-                ghoulPlayer.stop()
+                //Toast.makeText(this, "Stop", Toast.LENGTH_SHORT).show()
+                ghoulPlayer.pause()
+                onPause.value = true
                 //ghoulPlayer = MediaPlayer.create(baseContext, ghoulMusics[0])
             }
         }
