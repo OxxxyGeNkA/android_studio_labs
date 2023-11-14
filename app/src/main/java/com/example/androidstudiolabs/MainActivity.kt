0ee127313978a1efc4675dc3f62eefac1f6068ba
OxxxyGeN <valeratoltek@gmail.com>
2023-11-14 10:38:12 +0300

    Metanit

--- app/src/main/java/com/example/androidstudiolabs/MainActivity.kt
+++ app/src/main/java/com/example/androidstudiolabs/MainActivity.kt
@@ -1,43 +1,109 @@
 package com.example.androidstudiolabs
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CutCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.androidstudiolabs.ui.theme.AndroidStudioLabsTheme
 
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 class MainActivity : ComponentActivity() {
+    @SuppressLint("UnrememberedMutableState")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            AndroidStudioLabsTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+            var route = remember {
+                mutableStateOf(ScreenNames.LOGIN.name)
+            }
+            Column(modifier = Modifier.fillMaxSize()) {
+                Box(
+                    modifier = Modifier
+                        .weight(1f)
+                        .padding(16.dp)
+                ) {
+                    when (route.value) {
+                        ScreenNames.LOGIN.name -> LoginScreen()
+                        ScreenNames.GAME.name -> GameScreen()
+                        ScreenNames.MAP.name -> MapScreen()
+                        ScreenNames.RESULTS.name -> ResultsScreen()
+                    }
+                }
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp),
+                    horizontalArrangement = Arrangement.SpaceEvenly
+                ) {
+                    NavButton(ScreenNames.LOGIN.name, route)
+                    NavButton(ScreenNames.GAME.name, route)
+                    NavButton( ScreenNames.MAP.name, route)
+                    NavButton(ScreenNames.RESULTS.name, route)
                 }
             }
         }
     }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-            text = "Hello $name!",
-            modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AndroidStudioLabsTheme {
-        Greeting("Android")
+    //функция для отображения нижних кнопок
+    @Composable
+    fun NavButton(text: String, f: MutableState<String>) {
+        Button(onClick = { f.value=text}) {
+            Text(text = text)
+        }
     }
+    @Composable
+    fun LoginScreen() {
+        CardScreen(ScreenNames.LOGIN.toString())
+    }
+    @Composable
+    fun GameScreen() {
+        CardScreen(ScreenNames.GAME.toString())
+    }
+    @Composable
+    fun MapScreen() {
+        CardScreen(ScreenNames.MAP.toString())
+    }
+
+    @Composable
+    fun ResultsScreen() {
+        CardScreen(ScreenNames.RESULTS.toString())
+    }
+    //карточки для экранов
+    @Composable
+    fun CardScreen(screenName: String) {
+        val cardPaddingModifier = Modifier
+            .fillMaxSize()
+            .padding(20.dp)
+        Card(
+            shape = CutCornerShape(30.dp),
+            modifier = cardPaddingModifier,
+            elevation = CardDefaults.cardElevation(defaultElevation = 8.dp)
+        ) {
+
+            Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Center) {
+
+                Text(
+                    screenName
+                )
+            }
+        }
+    }
+}
+//энум для хранения названий экранов
+enum class ScreenNames{
+    LOGIN, GAME, MAP, RESULTS
 }
