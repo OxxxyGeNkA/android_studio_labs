4430077e31c263d462ab87df99de2588e985d6ca
OxxxyGeN <valeratoltek@gmail.com>
2023-11-23 18:00:12 +0300

    Metanit

--- app/src/main/java/com/example/androidstudiolabs/MainActivity.kt
+++ app/src/main/java/com/example/androidstudiolabs/MainActivity.kt
@@ -6,17 +6,14 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.clickable
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.lifecycle.viewmodel.compose.viewModel
+
 @Composable
 fun Screen0() {
-
     var i by remember {
         mutableStateOf(0)
     }
@@ -27,7 +24,6 @@ fun Screen0() {
 }
 
 class MainActivity : ComponentActivity() {
-    // val viewModel: MyViewModel1 by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
