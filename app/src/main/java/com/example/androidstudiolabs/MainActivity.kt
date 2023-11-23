3d3335784dedfed893b3d4572328cfb34d839c02
OxxxyGeN <valeratoltek@gmail.com>
2023-11-23 18:47:59 +0300

    Metanit

--- app/src/main/java/com/example/androidstudiolabs/MainActivity.kt
+++ app/src/main/java/com/example/androidstudiolabs/MainActivity.kt
@@ -1,43 +1,62 @@
 package com.example.androidstudiolabs
 
+import android.database.Cursor
+import android.database.sqlite.SQLiteDatabase
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.ScrollState
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.androidstudiolabs.ui.theme.AndroidStudioLabsTheme
-
+import androidx.compose.ui.unit.dp
+@ExperimentalMaterial3Api
 class MainActivity : ComponentActivity() {
+    lateinit var db: SQLiteDatabase
     override fun onCreate(savedInstanceState: Bundle?) {
+        db = openOrCreateDatabase("P11.db", MODE_PRIVATE, null);
+        db.execSQL("CREATE TABLE IF NOT EXISTS GAMES (id INTEGER PRIMARY KEY, name TEXT, cost REAL, year INTEGER)")
+        db.execSQL("INSERT OR IGNORE INTO GAMES(name, cost, year) VALUES ('GTA V', 2547.35, 2013), ('Project Zomboid', 545.44, 2017), ('DOOM Eternal', 3210.55, 2019)")
+        val query: String = "SELECT * FROM GAMES;"
+
+        val cursor: Cursor = db.rawQuery(query, null)
+        var games = buildString {
+            while (cursor.moveToNext()) {
+                var id = cursor.getInt(0)
+                var name = cursor.getString(1)
+                var cost: Double = cursor.getDouble(2)
+                var year: Int = cursor.getInt(3)
+                append("$id \t $name \t $cost \t $year\n")
+                Log.d(
+                    "P11", "NAME: $name COST: $cost YEAR: $year \n!"
+                )
+            }
+        }
         super.onCreate(savedInstanceState)
         setContent {
-            AndroidStudioLabsTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
-                }
+            Card(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .fillMaxHeight(0.9f)
+                    .padding(20.dp),
+                elevation = CardDefaults.cardElevation(
+                    defaultElevation = 8.dp
+                )
+            ) {
+                Text(games, modifier = Modifier.verticalScroll(ScrollState(1)))
             }
         }
     }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-            text = "Hello $name!",
-            modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AndroidStudioLabsTheme {
-        Greeting("Android")
+    override fun onDestroy() {
+        db.close()
+        deleteDatabase("P11.db")
+        super.onDestroy()
     }
 }
