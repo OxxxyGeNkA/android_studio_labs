44d03b1367ee08e29beaf08c27ab3a590df17ee3
OxxxyGeN <valeratoltek@gmail.com>
2023-11-24 00:23:37 +0300

    Metanit

--- app/src/main/java/com/example/androidstudiolabs/MainActivity.kt
+++ app/src/main/java/com/example/androidstudiolabs/MainActivity.kt
@@ -5,23 +5,35 @@ import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.sp
+import kotlin.math.abs
 
 @Composable
 fun Screen4() {
     var i by rememberSaveable {
         mutableStateOf(0)
     }
-    Text(
-        text = "Клики: $i",
-        modifier = Modifier.clickable(onClick = { i++ } )
-    )
+    val size = 20 + abs(i % 40 - 20) * 2
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = "Клики: $i",
+            modifier = Modifier.clickable(onClick = { i++ } ),
+            fontSize = size.sp
+        )
+    }
 }
 class MainActivity : ComponentActivity() {
     private val TAG = "♂GAYSEXXX♂"
