ca610df942888a63e7bf1c3c1145676ec2b278c9
OxxxyGeN <valeratoltek@gmail.com>
2023-11-24 01:11:05 +0300

    Metanit

--- app/src/main/java/com/example/androidstudiolabs/MainActivity.kt
+++ app/src/main/java/com/example/androidstudiolabs/MainActivity.kt
@@ -1,43 +1,79 @@
 package com.example.androidstudiolabs
 
+import com.github.javafaker.Faker
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.OutlinedCard
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.toMutableStateList
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.androidstudiolabs.ui.theme.AndroidStudioLabsTheme
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+data class City(
+    val name: String,
+    val temp: Double
+)
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val faker = Faker()
+        val citiz: MutableList<City> = (1..50000).map {
+                City(
+                    faker.address().city(),
+                    String.format("%.1f", faker.random().nextDouble()).toDouble()
+                )
+            }.toMutableStateList()
         setContent {
-            AndroidStudioLabsTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
-                }
-            }
+            MyScreen(citiz)
         }
     }
 }
-
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-            text = "Hello $name!",
-            modifier = modifier
-    )
+fun CityItem(cityModel: City) {
+    OutlinedCard(
+        modifier = Modifier
+            .fillMaxWidth(), elevation = CardDefaults.cardElevation(10.dp)
+    ) {
+        Row(horizontalArrangement = Arrangement.SpaceAround) {
+            Text(
+                text = cityModel.name,
+
+                modifier = Modifier.fillMaxWidth(0.7f),
+
+                fontSize = 20.sp,
+                textAlign = TextAlign.Center
+            )
+            Text(text = cityModel.temp.toString(), fontSize = 20.sp, textAlign = TextAlign.Right)
+        }
+    }
 }
-
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    AndroidStudioLabsTheme {
-        Greeting("Android")
+fun MyScreen(citiz: List<City>) {
+    LazyColumn(
+        verticalArrangement = Arrangement.spacedBy(3.dp),
+        contentPadding = PaddingValues(16.dp),
+        modifier = Modifier
+            .border(3.dp, Color.Gray)
+            .padding(10.dp)
+
+    ) {
+        items(citiz) {
+            CityItem(it)
+        }
     }
 }
