abc41c403221fb47d5a18771d0e3b1efe5a07bfe
OxxxyGeN <valeratoltek@gmail.com>
2023-12-01 09:15:37 +0300

    Metanit

--- app/src/main/java/com/example/androidstudiolabs/MainActivity.kt
+++ app/src/main/java/com/example/androidstudiolabs/MainActivity.kt
@@ -17,6 +17,7 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+
 @ExperimentalMaterial3Api
 class MainActivity : ComponentActivity() {
     lateinit var db: SQLiteDatabase
